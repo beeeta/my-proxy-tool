ea20d2e64481e693daf0d721d1fdbe93fa6614f2
betalun@qq.com <betalun@qq.com>
2018-02-08 18:01:26 +0800

    fire

--- vpn_pool/spiders/byday.py
+++ vpn_pool/spiders/byday.py
@@ -10,12 +10,30 @@ class EveryDaySpider(scrapy.Spider):
     start_urls = ['https://proxy.coderbusy.com/article/category/daily-proxy.aspx/']
 
     def parse(self, response):
+        # node_hrefs = response.xpath('//a[@class="tile waves-attach waves-effect"]/@href').extract()
+        # for href in node_hrefs:
+        #     sub_href = response.urljoin(href)
+        #     yield scrapy.Request(sub_href,callback=self.parse_proxy_content)
+        yield from self.parse_per_page(response)
+
+        # 获取其他页的数据
+        page_url = response.xpath('//a[@class="btn btn-flat waves-attach/@href"]').extract()[-1]
+        page_url_index = page_url.split('=')
+        if len(page_url_index) == 2 and isinstance(page_url_index[1],int):
+            total_page = page_url_index[1]
+            for i in range(2,total_page+1):
+                relative_page=page_url_index[0]+str(i)
+                yield scrapy.Request(response.urljoin(relative_page),callback=self.parse_per_page)
+
+    # 解析页数信息
+    def parse_per_page(self,response):
         node_hrefs = response.xpath('//a[@class="tile waves-attach waves-effect"]/@href').extract()
         for href in node_hrefs:
             sub_href = response.urljoin(href)
-            yield scrapy.Request(sub_href,callback=self.parse_sub)
+            yield scrapy.Request(sub_href, callback=self.parse_proxy_content)
 
-    def parse_sub(self, response):
+    # 解析代理详情页面
+    def parse_proxy_content(self, response):
         raw_content = response.xpath('//div[@class="card-inner"]/div[2]').extract_first()
         items = raw_content.split('<br>')
         for i in items:
